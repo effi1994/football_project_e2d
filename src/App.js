d3d2c1da21026b2010c3819b7dfe8c1066e9076d
David <shai@gmail.com>
2022-12-18 20:30:48 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,53 +1,13 @@
+import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header";
-import {Container} from "@mui/material";
-import {Route, Routes, useLocation} from "react-router-dom"
-import LiveResults from "./pages/LiveResults";
-import LeagueTable from "./pages/LeagueTable";
-import LeagueTableLive from "./pages/LeagueTableLive";
-import Login from "./pages/Login";
-import {useEffect, useState} from "react";
-import {getUser,getToken} from "./services/userAtuhService";
+import Wrapper from "./components/Wrapper";
 
 function App() {
-    //To Do: Implement a login page,
-    //To Do: Implement a logout button,
-    //To Do: Implement a user profile page,
-    //To Do: Implement a user profile page,
-    //To Do: Implement a user profile page,
-    //To Do: Implement a user profile page,
-    //To Do: Implement a user profile page,
-    //To Do: Implement a user profile page,
-    //To Do: Implement a user profile page,
-    //effi OVEDDD!!!!!!!!3342
-
-    let location = useLocation()
- const [token, setToken] = useState(null);
-    useEffect( () => {
-       if (getToken()){
-           setToken(getToken());
-       }else {
-           setToken(null);
-       }
-    })
-
-    const handleToken = (token) => {
-        setToken(token);
-    }
-
     return (
-        <>
-          <Header path={location.pathname} token={token}/>
-          <Container>
-              <Routes>
-                  <Route path={"/"} element={<LiveResults/>}/>
-                  <Route path={"/league-table"} element={<LeagueTable/>}/>
-                  <Route path={"/league-table-live"} element={<LeagueTableLive/>}/>
-                  <Route path={"/login"} element={<Login token={token} onToken={handleToken}/>} />
-              </Routes>
-          </Container>
-        </>
-  );
+        <div className="App">
+            <Wrapper/>
+        </div>
+    );
 }
 
 export default App;
