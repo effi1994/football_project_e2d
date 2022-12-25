ceae9eb34c39ff37aafe7f001ad50a08cbf4751a
David <shai@gmail.com>
2022-12-25 20:05:12 +0200

    updated

--- src/pages/LeagueTableLive.jsx
+++ src/pages/LeagueTableLive.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import {useEffect, useState} from "react";
 import { calculateTableLeagueLive, tableLeagueLive} from "../services/CalculationTableService";
 import {getTeams} from "../services/teamService";
-import {getAllGames} from "../services/gameService";
+import {getAllGames,liveGamesInDb,getLiveGames} from "../services/gameService";
 import  LeaguesList from "../components/LeaguesList";
+import {Alert} from "@mui/material";
 
 const LeagueTableLive = () => {
     const [lifeTable,setLifeTable] = React.useState([]);
 
     useEffect(()=>{
         getTeams();
+        getLiveGames();
         getAllGames();
         calculateTableLeagueLive();
         setLifeTable(tableLeagueLive);
         const interval = setInterval(() => {
             getTeams();
+            getLiveGames();
             getAllGames();
             calculateTableLeagueLive();
             setLifeTable(tableLeagueLive);
@@ -22,10 +25,13 @@ const LeagueTableLive = () => {
         return () => clearInterval(interval);
     },[])
 
-
     return (
         <div>
-            <LeaguesList leagues={lifeTable} mode={"leagueTable"}/>
+            {
+                liveGamesInDb.length > 0 ? <LeaguesList leagues={lifeTable} mode={"leagueTable"}/> :
+                    <Alert sx={{marginTop:10}} severity="warning">There is no live games yet</Alert>
+
+            }
         </div>
     );
 
